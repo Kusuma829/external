dd667bd8a2c7147a9db5fb99635048636bff84fb
Kusuma829 <245122749006@mvsrec.edu.in>
2025-07-25 10:04:39 +0530

    Add files via upload

---
+++ STUD.java
@@ -0,0 +1,11 @@
+public class STUD{
+public static void main(String args[])
+{
+String name="kusuma";
+int Rollno=006;
+String dept="CSE";
+System.out.println("Name:"+name);
+System.out.println("RollNumber:"+name);
+System.out.println("Department:"+dept);
+}
+}
